88361ec846e87865b5e37f9f76ada16b245aa59c
Mike Mitterer <office@mikemitterer.at>
2024-08-21 10:30:03 +0200

    KA, zu lange her0

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,6 @@
 module.exports = {
-  preset: '@vue/cli-plugin-unit-jest/presets/typescript-and-babel'
+  preset: '@vue/cli-plugin-unit-jest/presets/typescript-and-babel',
+
+  // https://www.npmjs.com/package/jest-extended#setup
+  "setupFilesAfterEnv": [ "jest-extended/all" /*, './test/jest.setup.js' */ ],
 }
--- rollup.config.js
+++ rollup.config.js
@@ -15,120 +15,146 @@ import vue from 'rollup-plugin-vue' // Handle .vue SFC files
 import resolve from '@rollup/plugin-node-resolve'
 import del from 'rollup-plugin-delete'
 import css from 'rollup-plugin-css-porter'
-import scss from 'rollup-plugin-scss'
+import sass from 'rollup-plugin-sass'
 import babel from '@rollup/plugin-alias'
 import { terser } from "rollup-plugin-terser"
 import alias from '@rollup/plugin-alias'
 import image from '@rollup/plugin-image'
 
-const name = "MobiAdComponents";
-const lib = 'mobiad-components';
+const name = "MobiAdComponents"
+const lib = 'mobiad-components'
 
 const globals = {
-    'tslib': 'tslib',
-    'vue-property-decorator': 'vuePropertyDecorator',
-    '@mmit/logging' : 'logging',
-    'spark-md5': 'SparkMD5',
-};
+  'tslib': 'tslib',
+  'vue-property-decorator': 'vuePropertyDecorator',
+  '@mmit/logging': 'logging',
+  'spark-md5': 'SparkMD5'
+}
 
 export default {
-    input: 'src/index.ts', // our source file
+  input: 'src/index.ts', // our source file
 
-    output: [
-        {
-            // Keep the bundle as an ES module file, suitable for other bundlers
-            // and inclusion as a <script type=module> tag in modern browsers
-            name,
-            file: `lib/${lib}.esm.js`,
-            format: 'esm', // the preferred format
-            sourcemap: true,
-        },
-        {
-            // Universal Module Definition, works as amd, cjs and iife all in one
-            name,
-            file: `lib/${lib}.umd.js`,
-            format: 'umd',
-            sourcemap: true,
-            globals,
-            exports: 'named',
-        },
-        {
-            // A self-executing function, suitable for inclusion as a <script> tag.
-            // (If you want to create a bundle for your application, you probably want to use this.)
-            name,
-            file: `lib/${lib}.min.js`,
-            format: 'iife',
-            sourcemap: true,
-            plugins: [terser()],
-            globals,
-            exports: 'named',
-        },
-        {
-            // CommonJS, suitable for Node and other bundlers
-            name,
-            file: `lib/${lib}.cjs.js`,
-            format: 'cjs',
-            sourcemap: true,
-            globals,
-            exports: 'named',
-        },
-    ],
-    // Unterdrückt die Fehlermeldung "Mixing named and default exports"
-    // (Kommt von index.ts - da wird das so gemacht)
-    // exports: 'named',
+  output: [
+    {
+      // Keep the bundle as an ES module file, suitable for other bundlers
+      // and inclusion as a <script type=module> tag in modern browsers
+      name,
+      file: `lib/${lib}.esm.js`,
+      format: 'esm', // the preferred format
+      sourcemap: true
+    },
+    // {
+    //     // Behält die directory struktur bei...
+    //     //      package.json: {
+    //     //          "module": "./lib/esm-split/index.js",
+    //     //          "typings": "./lib/esm-split/index.d.ts",
+    //     //      }
+    //     name,
+    //     dir: `lib/esm-split`,
+    //     format: 'esm', // the preferred format
+    //     sourcemap: true,
+    //     preserveModules: true,
+    //     preserveModulesRoot: 'src',
+    //     plugins: [terser({
+    //        compress: {
+    //            unused: false,
+    //            collapse_vars: false
+    //       },
+    //        ecma: 2019,
+    //        // safari10: true,
+    //     })]
+    // },
+    // {
+    //     // Universal Module Definition, works as amd, cjs and iife all in one
+    //     name,
+    //     file: `lib/umd/index.js`,
+    //     format: 'umd',
+    //     sourcemap: true,
+    //     globals,
+    //     exports: 'named',
+    // },
+    {
+      // A self-executing function, suitable for inclusion as a <script> tag.
+      // (If you want to create a bundle for your application, you probably want to use this.)
+      name,
+      file: `lib/${lib}.min.js`,
+      format: 'iife',
+      sourcemap: true,
+      plugins: [terser()],
+      globals,
+      exports: 'named'
+    },
+    {
+      // CommonJS, suitable for Node and other bundlers
+      name,
+      file: `lib/${lib}.cjs.js`,
+      format: 'cjs',
+      sourcemap: true,
+      globals,
+      exports: 'named'
+    }
+  ],
+  // Unterdrückt die Fehlermeldung "Mixing named and default exports"
+  // (Kommt von index.ts - da wird das so gemacht)
+  // exports: 'named',
 
-    external: [
-        ...Object.keys(pkg.dependencies || {}),
-        // "vue-class-component",
-        // "vue-property-decorator",
-        "tslib",
-        "vue",
-        "vuex",
-        "vuex-class",
-        "vuetify",
-        "vuetify/lib",
-    ],
-    plugins: [
-        alias({
-            '@': __dirname + '/src/main'
-        }),
-        typescript({
-            typescript: require('typescript'),
-            objectHashIgnoreUnknownHack: true,
-            module: 'esnext',
+  external: [
+    ...Object.keys(pkg.dependencies || {}),
+    // "vue-class-component",
+    // "vue-property-decorator",
+    "tslib",
+    "vue",
+    "vuex",
+    "vuex-class",
+    "vuetify",
+    "vuetify/lib"
+  ],
+  plugins: [
+    alias({
+      '@': __dirname + '/src/main'
+    }),
 
-            tsconfig: "tsconfig.json",
-            tsconfigOverride: { exclude: [
-                "node_modules", "tests",
-                    "src/main.ts",
-                    "src/router.ts",
-                    "src/plugins",
-                    "src/simulations",
-                    "src/views",
-                ] }
-        }),
+    typescript({
+      // objectHashIgnoreUnknownHack: true,
+      typescript: require('typescript'),
+      module: 'esnext',
 
-        babel({
-            exclude: 'node_modules/**',
-            sourceMaps: 'both'
-        }),
-        
-        // [Rollup Plugin Vue](https://rollup-plugin-vue.vuejs.org/)
-        vue({
-            css: true, // Dynamically inject css as a <style> tag
-            compileTemplate: true, // Explicitly convert template to render function
-            defaultLang: { script: 'ts' },
-        }),
-        scss(),
-        image(),
-        css(),
-        resolve(),
-        // terser() // minifies generated bundles
+      tsconfig: "tsconfig.json",
+      tsconfigOverride: {
+        exclude: [
+          "node_modules",
+          "tests",
+          "src/main.ts",
+          "src/router.ts",
+          "src/plugins",
+          "src/simulations",
+          "src/views"
+        ]
+      }
+    }),
 
-        commonjs(),
-        del({
-            targets: 'lib/main.d.ts',
-            hook: 'generateBundle'
-        })
-    ]
-};
+    babel({
+      exclude: 'node_modules/**',
+      sourceMaps: 'both'
+    }),
+
+    // [Rollup Plugin Vue](https://rollup-plugin-vue.vuejs.org/)
+    vue({
+      preprocessStyles: true,
+      // css: true, // Dynamically inject css as a <style> tag
+      // compileTemplate: true, // Explicitly convert template to render function
+      defaultLang: {script: 'ts'}
+    }),
+    sass(),
+    image(),
+    css(),
+    resolve(),
+    // terser() // minifies generated bundles
+
+    commonjs(),
+    del({
+      targets: 'lib/main.d.ts',
+      hook: 'generateBundle'
+    })
+  ]
+}
